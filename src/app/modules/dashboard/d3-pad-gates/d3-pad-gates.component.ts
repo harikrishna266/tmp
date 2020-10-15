46514e53e4614f4ea1120bab6a6b5b9a060210ce
hari krishna <hari@eleet.ca>
2020-10-15 19:07:12 +0530

    sankey

--- src/app/modules/dashboard/d3-pad-gates/d3-pad-gates.component.ts
+++ src/app/modules/dashboard/d3-pad-gates/d3-pad-gates.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import { select, tree, zoom, hierarchy, linkHorizontal, event } from 'd3';
+import { select, zoom, hierarchy, linkHorizontal, event } from 'd3';
+import {sankey} from 'd3-sankey';
 
 
 @Component({
@@ -18,6 +19,7 @@ export class D3PadGatesComponent implements OnInit, AfterViewInit {
 	public width;
 	public pads = 15;
 	public  margin = { top: 0, right: 50, bottom: 0, left: 75};
+	public graph;
 
 	constructor(private el: ElementRef) { }
 
@@ -28,93 +30,108 @@ export class D3PadGatesComponent implements OnInit, AfterViewInit {
 		this.setinitialSVG();
 		// this.createZoom();
 		this.getData();
-		this.renderTree();
-		
+		this.renderGraph();
 	}
-
-	// createZoom(): void {
-	// 	this.zoomG = this.svg
-	// 		.attr('width', this.width)
-	// 		.attr('height', this.height)
-	// 		.append('g');
-
-	// 	const g = this.zoomG.append('g')
-	// 		.attr('transform', `translate(${this.margin.left},${this.margin.top})`);
-
-	// 	this.svg.call(zoom().on('zoom', () => {
-	// 		this.zoomG.attr('transform', event.transform);
-	// 	}));
-	// }
 
 	setinitialSVG(): void {
 		this.height = this.el.nativeElement.clientHeight;
 		this.width = this.el.nativeElement.clientHeight;
+		
 		console.log(this.height, this.width);
 		this.svg = select('svg');
 		this.svg
 			.attr('width', this.width)
 			.attr('height', this.height)
-			.append('g');
+			.append('g')
+			.attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-		const innerWidth = this.width - this.margin.left - this.margin.right;
-		const innerHeight = this.height - this.margin.top - this.margin.bottom;
-
-		this.treeLayout = tree().size([innerWidth,   innerHeight]);
+		const sankeyObj = sankey()
+		.nodeWidth(36)
+		.nodePadding(290)
+		.size([this.width, this.height]);
 	}
 
 	getData(): void {
-		const flights = [];
-		
-		this.data = data;
-		console.log(this.data);
+		this.graph = data;
 	}
 
-	renderTree(): void {
-		const root = hierarchy(this.data);
-		const links = this.treeLayout(root).links();
-		const linkPathGenerator = linkHorizontal().x(d => d.y).y(d => d.x);
-		this.svg.selectAll('path').data(links).enter().append('path').attr('d', linkPathGenerator);
-
-		this.svg.selectAll('text')
-		.data(root.descendants())
-		.enter().append('text')
-			.attr('x', d => d.y)
-			.attr('y', d => d.x)
-			.attr('text-anchor', d => d.children ? 'middle' : 'start')
-			.text(d => d.data.data.id);
+	renderGraph(): void {
+		sankey()
+		.nodeWidth(36)
+		.nodePadding(290)
+		.size([this.width, this.height]);
 
 
-		console.log(this.svg.selectAll('text')
-		.data(root.descendants())
-		.enter())
-		// .append('image')
-		// .attr("xlink:href", function(d) { return 'https://user-images.githubusercontent.com/3151700/69676637-0fd6d680-1099-11ea-85ab-2a9e68229328.png'})
+		this.svg.append('g')
+		.selectAll('.link')
+		.data(this.graph.links)
+		.enter()
+		.append("path")
+		.attr("class", "link")
+		.attr("d", sankey.link() )
+		.style("stroke-width", function(d) { return Math.max(1, d.dy); })
+		.sort(function(a, b) { return b.dy - a.dy; });
 	}
 }
 
 export const data = {
-	data: {
-		id: 'Pads'
-	},
-	children: [
+	nodes: [
 		{
-			data: {
-				id: 'wait 1',
-			},
-			children: [
-				{
-					data:  {
-						id: 'wait 2'
-					},
-					children: [
-						{
-							data:  {
-								id: 'wait 3'
-							}
-						}
-					]
-				}
-			]
+			node: 0,
+			name: 'node0'
+		},
+		{
+			node: 1,
+			name: 'node1'
+		},
+		{
+			node: 2,
+			name: 'node2'
+		},
+		{
+			node: 3,
+			name: 'node3'
+		},
+		{
+			node: 4,
+			name: 'node4'
+		}
+	],
+	links: [
+		{
+			source: 0,
+			target: 2,
+			value: 2
+		},
+		{
+			source: 1,
+			target: 2,
+			value: 2
+		},
+		{
+			source: 1,
+			target: 3,
+			value: 2
+		},
+		{
+			source: 0,
+			target: 4,
+			value: 2
+		},
+		{
+			source: 2,
+			target: 3,
+			value: 2
+		},
+		{
+			source: 2,
+			target: 4,
+			value: 2
+		},
+		{
+			source: 3,
+			target: 4,
+			value: 4
 		}
 	]
-};
+}
